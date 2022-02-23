f9db219b045973ae8aae62590fdb6732bfae68ef
squak <squakrazv@gmail.com>
2022-02-24 02:20:52 +0300

    feat: set build options

---
+++ devind-rich-text-editor/index.ts
@@ -0,0 +1,1 @@
+export { default as RichTextEditor } from './src/components/RichTextEditor.vue'
--- devind-rich-text-editor/vite.config.ts
+++ devind-rich-text-editor/vite.config.ts
@@ -1,5 +1,22 @@
+import path from 'path'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from 'vite'
 
-export default {
-    plugins: [createVuePlugin()]
-}
+export default defineConfig({
+    plugins: [createVuePlugin()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'index.ts'),
+            name: 'DevindRichTextEditor',
+            fileName: (format => `devind-rich-text-editor.${format}.js`)
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    }
+})
