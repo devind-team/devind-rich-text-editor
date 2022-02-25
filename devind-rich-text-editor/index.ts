9a8f539c1eb9f8016d4470fd08511e32978a5ea7
squak <squakrazv@gmail.com>
2022-02-25 08:34:07 +0300

    fix: export the plugin along with the RichTextEditor` component

--- devind-rich-text-editor/index.ts
+++ devind-rich-text-editor/index.ts
@@ -1,1 +1,10 @@
-export { default } from './src/components/RichTextEditor.vue'
+import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+
+export { default as RichTextEditor } from './src/components/RichTextEditor.vue'
+
+export default {
+  install() {
+    Vue.use(VueCompositionAPI)
+  }
+}
