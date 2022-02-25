5937c67b5f78efcc9073817e58d6091347608a4f
squak <squakrazv@gmail.com>
2022-02-25 09:58:42 +0300

    fix: inject css into es module

--- devind-rich-text-editor/vite.config.ts
+++ devind-rich-text-editor/vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
-import ScriptSetup from 'unplugin-vue2-script-setup/vite'
+import scriptSetup from 'unplugin-vue2-script-setup/vite'
+import libCss from 'vite-plugin-libcss'
 
 export default defineConfig({
-  plugins: [createVuePlugin(), ScriptSetup()],
+  plugins: [createVuePlugin(), scriptSetup(), libCss()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'index.ts'),
@@ -12,7 +13,6 @@ export default defineConfig({
       fileName: format => `devind-rich-text-editor.${format}.js`
     },
     rollupOptions: {
-      plugins: [ScriptSetup()],
       external: ['vue'],
       output: {
         globals: {
