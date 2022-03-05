f2553b8e4bce711e44d9c469ee1849f8cbcd63e3
squak <squakrazv@gmail.com>
2022-03-05 10:30:44 +0300

    fix: fix Vuetify support

--- index.ts
+++ index.ts
@@ -1,10 +1,1 @@
-import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api'
-
 export { default as RichTextEditor } from './src/components/RichTextEditor.vue'
-
-export default {
-  install() {
-    Vue.use(VueCompositionAPI)
-  }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -6,36 +6,39 @@ import libCss from 'vite-plugin-libcss'
 import Components from 'unplugin-vue-components/vite'
 import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: '@/',
-        replacement: `${path.resolve(__dirname, './src')}/`
-      }
-    ]
-  },
-  plugins: [
-    createVuePlugin(),
-    scriptSetup(),
-    libCss(),
-    Components({
-      resolvers: [VuetifyResolver()]
-    })
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
-      name: 'DevindRichTextEditor',
-      fileName: format => `devind-rich-text-editor.${format}.js`
+export default defineConfig(({ command }) => {
+  const plugins = [createVuePlugin(), scriptSetup(), libCss()]
+  if (command === 'serve') {
+    plugins.push(
+      Components({
+        resolvers: [VuetifyResolver()]
+      })
+    )
+  }
+  return {
+    resolve: {
+      alias: [
+        {
+          find: '@/',
+          replacement: `${path.resolve(__dirname, './src')}/`
+        }
+      ]
     },
-    rollupOptions: {
-      external: ['vue', '@vue/composition-api', 'vuetify'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          '@vue/composition-api': 'VueCompositionAPI',
-          vuetify: 'Vuetify'
+    plugins,
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'index.ts'),
+        name: 'DevindRichTextEditor',
+        fileName: format => `devind-rich-text-editor.${format}.js`
+      },
+      rollupOptions: {
+        external: ['vue', '@vue/composition-api', 'vuetify'],
+        output: {
+          globals: {
+            vue: 'Vue',
+            '@vue/composition-api': 'VueCompositionAPI',
+            vuetify: 'Vuetify'
+          }
         }
       }
     }
